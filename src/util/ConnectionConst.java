0fa7e62f38a144673f52c7965a585c79b232d590
chaos0103 <lyt1228@naver.com>
2023-03-24 16:19:52 +0900

    feat: DB 연결 기능 구현

---
+++ src/util/ConnectionConst.java
@@ -0,0 +1,9 @@
+package util;
+
+public abstract class ConnectionConst {
+
+    public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    public static final String URL = "localhost:3306";
+    public static final String USER = "root";
+    public static final String PASSWORD = "12345678";
+}
