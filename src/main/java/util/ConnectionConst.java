f678a6b1c39fda2ea2a86c62f620c1b71d327ae7
chaos0103 <lyt1228@naver.com>
2023-03-25 10:16:29 +0900

    fix: 데이터베이스 URL 변경

--- src/main/java/util/ConnectionConst.java
+++ src/main/java/util/ConnectionConst.java
@@ -3,7 +3,7 @@ package util;
 public abstract class ConnectionConst {
 
     public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-    public static final String URL = "localhost:3306";
+    public static final String URL = "jdbc:mysql://localhost:3306/enjoy_trip?serverTimezone=UTC";
     public static final String USER = "root";
     public static final String PASSWORD = "12345678";
 }
