b6db5d81583d9ae4f94d79f3d56fba0d5fc1f44f
chaos0103 <lyt1228@naver.com>
2023-03-25 19:39:26 +0900

    test: ArticleRepository#save 테스트 작성

---
+++ src/test/java/article/repository/ArticleRepositoryTest.java
@@ -0,0 +1,45 @@
+package article.repository;
+
+import article.dto.ArticleDto;
+import member.Member;
+import member.dto.MemberAddDto;
+import member.repository.MemberJdbcRepository;
+import member.repository.MemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ArticleRepositoryTest {
+
+    private final ArticleRepository articleRepository = ArticleJdbcRepository.getArticleRepository();
+    private final MemberRepository memberRepository = MemberJdbcRepository.getMemberRepository();
+
+    @BeforeEach
+    void beforeEach() {
+        MemberAddDto memberAddDto = new MemberAddDto("ssafy", "12345678", "김싸피", "ssafy@ssafy.com", "01012345678", "광주5반", "010101", "1");
+        memberRepository.save(memberAddDto);
+    }
+
+    @AfterEach
+    void afterEach() {
+        articleRepository.clear();
+        memberRepository.clear();
+    }
+
+    @Test
+    @DisplayName("게시글 저장")
+    void save() {
+        //given
+        Member member = memberRepository.findByLoginId("ssafy").get();
+        ArticleDto articleDto = new ArticleDto(member.getMemberId(), "title", "content");
+
+        //when
+        int count = articleRepository.save(articleDto);
+
+        //then
+        assertThat(count).isEqualTo(1);
+    }
+}
