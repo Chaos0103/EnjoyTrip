6043ab7eaf4e17b972b6d2acd2ccc4bbba0074b5
chaos0103 <lyt1228@naver.com>
2023-03-25 19:40:06 +0900

    docs: 게시글 스키마 작성

--- docs/sql/schema.sql
+++ docs/sql/schema.sql
@@ -26,3 +26,15 @@ create table if not exists `CONTENT_TYPE`
     `content_type_id`   int         not null primary key,
     `content_type_name` varchar(10) not null
 );
+
+create table if not exists `ARTICLE`
+(
+    `article_id`         bigint        not null primary key auto_increment,
+    `member_id`          bigint        not null,
+    `title`              varchar(100)  not null,
+    `content`            varchar(1000) not null,
+    `hit`                int           not null default 0,
+    `created_date`       timestamp     not null default current_timestamp,
+    `last_modified_date` timestamp     not null default current_timestamp,
+    foreign key (`member_id`) references MEMBER (`member_id`)
+);
