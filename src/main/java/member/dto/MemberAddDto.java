0357ebb6bcf7b175c151859fc0be588d066e3920
chaos0103 <lyt1228@naver.com>
2023-03-25 12:50:25 +0900

    refactor: Builder 패턴 변경

--- src/main/java/member/dto/MemberAddDto.java
+++ src/main/java/member/dto/MemberAddDto.java
@@ -1,11 +1,11 @@
 package member.dto;
 
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class MemberAddDto {
+
     private String loginId;
     private String loginPw;
     private String username;
@@ -14,4 +14,16 @@ public class MemberAddDto {
     private String nickname;
     private String birth;
     private String gender;
+
+    @Builder
+    public MemberAddDto(String loginId, String loginPw, String username, String email, String phone, String nickname, String birth, String gender) {
+        this.loginId = loginId;
+        this.loginPw = loginPw;
+        this.username = username;
+        this.email = email;
+        this.phone = phone;
+        this.nickname = nickname;
+        this.birth = birth;
+        this.gender = gender;
+    }
 }
