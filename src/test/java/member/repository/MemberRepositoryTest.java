94188d5883726d1bd781b52d65f5ad4ca1837252
chaos0103 <lyt1228@naver.com>
2023-03-25 15:49:41 +0900

    test: MemberRepository 테스트 코드 작성
    - MemberRepository#findById
    - MemberRepository#findByLoginId
    - MemberRepository#findByEmail
    - MemberRepository#findByPhone
    - MemberRepository#findByNickname
    - MemberRepository#update
    - MemberRepository#remove

--- src/test/java/member/repository/MemberRepositoryTest.java
+++ src/test/java/member/repository/MemberRepositoryTest.java
@@ -2,19 +2,24 @@ package member.repository;
 
 import member.Member;
 import member.dto.MemberAddDto;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class MemberRepositoryTest {
 
-    private MemberRepository memberRepository = MemberJdbcRepository.getMemberRepository();
+    private final MemberRepository memberRepository = MemberJdbcRepository.getMemberRepository();
+
+    @BeforeEach
+    void beforeEach() {
+        MemberAddDto memberAddDto = new MemberAddDto("ssafy", "12345678", "김싸피", "ssafy@ssafy.com", "01012345678", "광주5반", "010101", "1");
+        memberRepository.save(memberAddDto);
+    }
 
     @AfterEach
     void afterEach() {
@@ -23,9 +28,9 @@ class MemberRepositoryTest {
 
     @Test
     @DisplayName("회원저장")
-    void create() {
+    void save() {
         //given
-        MemberAddDto memberAddDto = new MemberAddDto("ssafy", "1234", "김싸피", "ssafy@ssafy.com", "01012345678", "광주5반", "010101", "1");
+        MemberAddDto memberAddDto = new MemberAddDto("ssafy", "12345678", "김싸피", "ssafy@ssafy.com", "01012345678", "광주5반", "010101", "1");
 
         //when
         memberRepository.save(memberAddDto);
@@ -33,5 +38,98 @@ class MemberRepositoryTest {
         //then
         Optional<Member> findMember = memberRepository.findByLoginId("ssafy");
         assertThat(findMember).isPresent();
+    }
+
+    @Test
+    @DisplayName("PK로 조회")
+    void findById() {
+        //given
+        Member member = memberRepository.findByLoginId("ssafy").get();
+
+        //when
+        Optional<Member> findMember = memberRepository.findById(member.getMemberId());
+
+        //then
+        assertThat(findMember).isPresent();
+    }
+
+    @Test
+    @DisplayName("로그인 아이디로 조회")
+    void findByLoginId() {
+        //given
+        String loginId = "ssafy";
+
+        //when
+        Optional<Member> findMember = memberRepository.findByLoginId(loginId);
+
+        //then
+        assertThat(findMember).isPresent();
+    }
+
+    @Test
+    @DisplayName("이메일로 조회")
+    void findByEmail() {
+        //given
+        String email = "ssafy@ssafy.com";
+
+        //when
+        Optional<Member> findMember = memberRepository.findByEmail(email);
+
+        //then
+        assertThat(findMember).isPresent();
+    }
+
+    @Test
+    @DisplayName("연락처로 조회")
+    void findByPhone() {
+        //given
+        String phone = "01012345678";
+
+        //when
+        Optional<Member> findMember = memberRepository.findByPhone(phone);
+
+        //then
+        assertThat(findMember).isPresent();
+    }
+
+    @Test
+    @DisplayName("닉네임으로 조회")
+    void findByNickname() {
+        //given
+        String nickname = "광주5반";
+
+        //when
+        Optional<Member> findMember = memberRepository.findByNickname(nickname);
+
+        //then
+        assertThat(findMember).isPresent();
+    }
+
+    @Test
+    @DisplayName("회원업데이트")
+    void update() {
+        //given
+        Member member = memberRepository.findByLoginId("ssafy").get();
+        member.changeNickname("SSAFY9기");
+        //when
+        memberRepository.update(member.getMemberId(), member);
+
+        //then
+        Optional<Member> findMember = memberRepository.findByNickname("SSAFY9기");
+        assertThat(findMember).isPresent();
+    }
+
+    @Test
+    @DisplayName("회원삭제")
+    void remove() {
+        //given
+        Member member = memberRepository.findByLoginId("ssafy").get();
+
+        //when
+        memberRepository.remove(member.getMemberId());
+
+        //then
+        Optional<Member> findMember = memberRepository.findById(member.getMemberId());
+        assertThat(findMember).isEmpty();
     }
 }
