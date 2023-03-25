41bb3b38e5528121420e0b201f5053aa3cbee2a6
chaos0103 <lyt1228@naver.com>
2023-03-25 13:34:25 +0900

    test: 생년월일 검증 테스트 구현

---
+++ src/test/java/common/validation/validator/signup/BirthValidatorTest.java
@@ -0,0 +1,64 @@
+package common.validation.validator.signup;
+
+import common.validation.dto.InvalidResponse;
+import member.dto.MemberAddDto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BirthValidatorTest {
+
+    private final BirthValidator validator = new BirthValidator();
+
+    @Test
+    @DisplayName("생년월일 검증")
+    void birthValidator() {
+        //given
+        MemberAddDto memberAddDto = MemberAddDto.builder()
+                .birth("010101")
+                .build();
+
+        //when
+        List<InvalidResponse> validate = validator.validate(memberAddDto);
+
+        //then
+        assertThat(validate).isEmpty();
+    }
+
+    @ParameterizedTest
+    @DisplayName("생년월일 길이 예외")
+    @CsvSource({"9911", "20010101"})
+    void exception_length(String birth) {
+        //given
+        MemberAddDto memberAddDto = MemberAddDto.builder()
+                .birth(birth)
+                .build();
+
+        //when
+        List<InvalidResponse> validate = validator.validate(memberAddDto);
+
+        //then
+        assertThat(validate).isNotEmpty();
+    }
+
+    @ParameterizedTest
+    @DisplayName("생년월일 타입 예외")
+    @CsvSource({"99.1.1", "O1O1O1"})
+    void exception_type(String birth) {
+        //given
+        MemberAddDto memberAddDto = MemberAddDto.builder()
+                .birth(birth)
+                .build();
+
+        //when
+        List<InvalidResponse> validate = validator.validate(memberAddDto);
+
+        //then
+        assertThat(validate).isNotEmpty();
+    }
+}
