20671f7d335bcada27a06a393acc1a011446b690
chaos0103 <lyt1228@naver.com>
2023-03-26 21:31:02 +0900

    refactor: FK 객체로 변경

--- src/main/java/article/Article.java
+++ src/main/java/article/Article.java
@@ -1,20 +1,34 @@
 package article;
 
-import lombok.AllArgsConstructor;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import member.Member;
 
 import java.time.LocalDateTime;
 
 @Getter
-@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Article {
 
     private Long articleId;
-    private Long memberId;
     private String title;
     private String content;
     private int hit;
     private LocalDateTime createdDate;
     private LocalDateTime lastModifiedDate;
 
+    private Member member;
+
+    @Builder
+    public Article(Long articleId, String title, String content, int hit, LocalDateTime createdDate, LocalDateTime lastModifiedDate, Member member) {
+        this.articleId = articleId;
+        this.title = title;
+        this.content = content;
+        this.hit = hit;
+        this.createdDate = createdDate;
+        this.lastModifiedDate = lastModifiedDate;
+        this.member = member;
+    }
 }
