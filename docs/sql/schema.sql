05b26eed4098bdfead4299ae8a41915ce9cc4ff6
chaos0103 <lyt1228@naver.com>
2023-03-26 09:22:54 +0900

    docs: Member 스키마 수정

--- docs/sql/schema.sql
+++ docs/sql/schema.sql
@@ -6,17 +6,19 @@ drop table if exists member;
 
 create table if not exists `MEMBER`
 (
-    `member_id`          bigint      not null primary key auto_increment,
-    `login_id`           varchar(20) not null,
-    `login_pw`           varchar(20) not null,
-    `username`           varchar(20) not null,
-    `email`              varchar(50) not null,
-    `phone`              varchar(11) not null,
-    `nickname`           varchar(10) not null,
-    `birth`              varchar(6) not null,
-    `gender`             varchar(1) not null,
-    `created_date`       timestamp   not null default current_timestamp,
-    `last_modified_date` timestamp   not null default current_timestamp
+    `member_id`                   bigint      not null primary key auto_increment,
+    `login_id`                    varchar(20) not null,
+    `login_pw`                    varchar(20) not null,
+    `username`                    varchar(20) not null,
+    `email`                       varchar(50) not null,
+    `phone`                       varchar(11) not null,
+    `birth`                       varchar(6)  not null,
+    `gender`                      varchar(1)  not null,
+    `nickname`                    varchar(10) not null,
+    `nickname_last_modified_date` timestamp   not null default current_timestamp,
+    `authority`                   varchar(10) not null default `client`,
+    `created_date`                timestamp   not null default current_timestamp,
+    `last_modified_date`          timestamp   not null default current_timestamp
 );
 
 drop table if exists content_type;
