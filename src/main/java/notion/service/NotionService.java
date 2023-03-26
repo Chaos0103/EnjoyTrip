1a356ce7b34a0c6894dae315f85f8b809504a193
chaos0103 <lyt1228@naver.com>
2023-03-26 16:39:05 +0900

    feat: 공지사항 수정 기능 구현

--- src/main/java/notion/service/NotionService.java
+++ src/main/java/notion/service/NotionService.java
@@ -5,4 +5,6 @@ import notion.dto.NotionDto;
 public interface NotionService {
 
     int addNotion(Long memberId, NotionDto notionDto);
+
+    int editNotion(Long notionId, Long memberId, NotionDto notionDto);
 }
--- src/main/java/notion/service/NotionServiceImpl.java
+++ src/main/java/notion/service/NotionServiceImpl.java
@@ -7,6 +7,7 @@ import member.Authority;
 import member.Member;
 import member.repository.MemberJdbcRepository;
 import member.repository.MemberRepository;
+import notion.Notion;
 import notion.dto.NotionDto;
 import notion.repository.NotionJdbcRepository;
 import notion.repository.NotionRepository;
@@ -50,5 +51,36 @@ public class NotionServiceImpl implements NotionService {
         }
 
         return notionRepository.save(memberId, notionDto);
+    }
+
+    @Override
+    public int editNotion(Long notionId, Long memberId, NotionDto notionDto) {
+        NotionValidation validation = new NotionValidation();
+
+        NotionRequest request = NotionRequest.builder()
+                .title(notionDto.getTitle())
+                .content(notionDto.getContent())
+                .build();
+        validation.validate(request);
+
+        Optional<Member> findMember = memberRepository.findById(memberId);
+        if (!findMember.isPresent()) {
+            throw new NotionException();
+        }
+
+        Member member = findMember.get();
+        if (member.getAuthority() != Authority.ADMIN) {
+            throw new NotionException();
+        }
+
+        Optional<Notion> findNotion = notionRepository.findById(notionId);
+        if (!findNotion.isPresent()) {
+            throw new NotionException();
+        }
+
+        Notion notion = findNotion.get();
+        notion.edit(notionDto.getTitle(), notionDto.getContent(), memberId);
+
+        return notionRepository.update(notionId, notion);
     }
 }
