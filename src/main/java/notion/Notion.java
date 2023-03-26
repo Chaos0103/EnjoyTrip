ba2964939c519be3f66fdf529c5930fa5e8d7c7e
chaos0103 <lyt1228@naver.com>
2023-03-26 16:15:25 +0900

    refactor: Notion 도메인 수정

--- src/main/java/notion/Notion.java
+++ src/main/java/notion/Notion.java
@@ -9,22 +9,24 @@ import java.time.LocalDateTime;
 public class Notion {
 
     private Long notionId;
-    private Long memberId;
     private String title;
     private String content;
     private int hit;
     private boolean top;
+    private Long createdBy;
+    private Long lastModifiedBy;
     private LocalDateTime createdDate;
     private LocalDateTime lastModifiedDate;
 
     @Builder
-    public Notion(Long notionId, Long memberId, String title, String content, int hit, boolean top, LocalDateTime createdDate, LocalDateTime lastModifiedDate) {
+    public Notion(Long notionId, String title, String content, int hit, boolean top, Long createdBy, Long lastModifiedBy, LocalDateTime createdDate, LocalDateTime lastModifiedDate) {
         this.notionId = notionId;
-        this.memberId = memberId;
         this.title = title;
         this.content = content;
         this.hit = hit;
         this.top = top;
+        this.createdBy = createdBy;
+        this.lastModifiedBy = lastModifiedBy;
         this.createdDate = createdDate;
         this.lastModifiedDate = lastModifiedDate;
     }
