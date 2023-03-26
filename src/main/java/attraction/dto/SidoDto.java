0a63b1ddc0662b411dead108af35fe71baa4ebed
chaos0103 <lyt1228@naver.com>
2023-03-27 00:06:51 +0900

    feat: 시도 전체 조회 기능 구현

---
+++ src/main/java/attraction/dto/SidoDto.java
@@ -0,0 +1,16 @@
+package attraction.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+public class SidoDto {
+    private Integer code;
+    private String name;
+
+    @Builder
+    public SidoDto(Integer code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+}
---
+++ src/main/java/attraction/service/SidoService.java
@@ -0,0 +1,10 @@
+package attraction.service;
+
+import attraction.dto.SidoDto;
+
+import java.util.List;
+
+public interface SidoService {
+
+    List<SidoDto> findAll();
+}
