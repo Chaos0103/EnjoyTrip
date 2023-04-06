5c5391c6e63d8369b4ec0a4b068bc33a2a21934c
leeyr0412 <leeyr0412@naver.com>
2023-04-06 15:18:34 +0900

    feat: 여행계획 저장기능 구현

--- src/main/java/tripplan/repository/PlanJdbcRepository.java
+++ src/main/java/tripplan/repository/PlanJdbcRepository.java
@@ -1,10 +1,14 @@
 package tripplan.repository;
 
+import member.Member;
+import tripplan.TripPlan;
 import util.DBConnectionUtil;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Optional;
 
 public class PlanJdbcRepository implements PlanRepository {
     private static final PlanRepository planRepository = new PlanJdbcRepository();
@@ -57,5 +61,41 @@ public class PlanJdbcRepository implements PlanRepository {
             dbConnectionUtil.close(pstmt, conn);
         }
         return count;
+    }
+
+    @Override
+    public Optional<TripPlan> findById(Long tripPlanId) {
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        TripPlan tripPlan = null;
+        try {
+            conn = dbConnectionUtil.getConnection();
+            String sql = "select * from trip_plan where trip_plan_id=?";
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setLong(1, tripPlanId);
+            rs = pstmt.executeQuery();
+            if (rs.next()) {
+                tripPlan = createTripPlan(rs);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            dbConnectionUtil.close(rs, pstmt, conn);
+        }
+        return Optional.ofNullable(tripPlan);
+    }
+
+    private TripPlan createTripPlan(ResultSet rs) throws SQLException {
+        return TripPlan.builder()
+                .id(rs.getLong("trip_plan_id"))
+                .member(Member.builder()
+                        .id(rs.getLong("member_id"))
+                        .build()
+                )
+                .title(rs.getString("title"))
+                .createdDate(rs.getTimestamp("created_date").toLocalDateTime())
+                .lastModifiedDate(rs.getTimestamp("last_modified_date").toLocalDateTime())
+                .build();
     }
 }
--- src/main/java/tripplan/repository/PlanRepository.java
+++ src/main/java/tripplan/repository/PlanRepository.java
@@ -1,7 +1,12 @@
 package tripplan.repository;
 
+import tripplan.TripPlan;
+
+import java.util.Optional;
+
 public interface PlanRepository {
     int addTripPlan(Long memberId, String title);
     int addDetailPlan(Long tripPlanId, int contentId);
+    Optional<TripPlan> findById(Long tripPlanId);
 }
 
