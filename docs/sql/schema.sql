83c2fd433641ca3d8234e753f573f79e0131dc5b
chaos0103 <lyt1228@naver.com>
2023-04-08 12:29:04 +0900

    docs: 핫플레이스 스키마 수정

--- docs/sql/schema.sql
+++ docs/sql/schema.sql
@@ -59,15 +59,16 @@ create table if not exists `NOTION`
 create table if not exists `HOT_PLACE`
 (
     `hot_place_id` bigint not null primary key auto_increment,
-    `title` varchar(50) not null,
+    `name` varchar(50) not null,
     `desc` varchar(500) not null,
     `hit` int not null default 0,
     `vote` int not null default 0,
-    `visited_date` varchar(8) not null,
+    `visited_date` varchar(10) not null,
     `upload_file_name` varchar(255) not null,
     `store_file_name` varchar(255) not null,
     `member_id` bigint not null,
     `content_id` int not null,
+    `content_type_id` int not null,
     `created_date` timestamp not null default current_timestamp,
     `last_modified_date` timestamp not null default current_timestamp,
     foreign key (`member_id`) references MEMBER (`member_id`),
