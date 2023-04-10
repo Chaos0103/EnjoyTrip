b4c1df230a4229488c0d2f9ac52e7b9ec0721ddb
Chaos0103 <lyt1228@naver.com>
2023-04-10 12:07:21 +0900

    test: 핫플레이스 삭제 기능 테스트 코드 작성

--- src/test/java/hotplace/repository/HotPlaceRepositoryTest.java
+++ src/test/java/hotplace/repository/HotPlaceRepositoryTest.java
@@ -2,31 +2,49 @@ package hotplace.repository;
 
 import hotplace.HotPlace;
 import hotplace.UploadFile;
+import hotplace.dto.HotPlaceSearch;
 import member.Member;
 import member.dto.MemberAddDto;
 import member.repository.MemberJdbcRepository;
 import member.repository.MemberRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static member.Authority.CLIENT;
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 class HotPlaceRepositoryTest {
 
     private final HotPlaceRepository hotPlaceRepository = HotPlaceJdbcRepository.getHotPlaceRepository();
     private final MemberRepository memberRepository = MemberJdbcRepository.getMemberRepository();
     private Long memberId;
+    private Long hotPlaceId;
 
     @BeforeEach
     void beforeEach() {
         memberRepository.save(new MemberAddDto("ssafy", "12345678", "김싸피", "ssafy@ssafy.com", "01012345678", "010101", "1", "광주5반", CLIENT));
         Member member = memberRepository.findByLoginId("ssafy").get();
         memberId = member.getId();
+
+        HotPlace hotPlace = HotPlace.builder()
+                .name("핫플레이스 이름")
+                .desc("핫플레이스 설명")
+                .visitedDate("2020-01-01")
+                .uploadFile(
+                        UploadFile.builder()
+                                .uploadFileName("uploadFileName.png")
+                                .storeFileName("storeFileName.png")
+                                .build()
+                )
+                .contentTypeId(11)
+                .build();
+        hotPlaceRepository.save(memberId, 125405, hotPlace);
+        List<HotPlace> findHotPlace = hotPlaceRepository.findByCondition(new HotPlaceSearch("", 1));
+        hotPlaceId = findHotPlace.get(0).getId();
     }
 
     @AfterEach
@@ -54,6 +72,17 @@ class HotPlaceRepositoryTest {
 
         //when
         int result = hotPlaceRepository.save(memberId, 125405, hotPlace);
+
+        //then
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("핫플레이스 삭제")
+    void remove() {
+        //given
+        //when
+        int result = hotPlaceRepository.remove(hotPlaceId);
 
         //then
         assertThat(result).isEqualTo(1);
