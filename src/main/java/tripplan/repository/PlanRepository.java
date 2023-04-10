cf494092e4f94ca480dcc13ef2d7a5b0fa5fff9f
Chaos0103 <lyt1228@naver.com>
2023-04-10 15:35:53 +0900

    style: format 맞춤

--- src/main/java/tripplan/repository/PlanRepository.java
+++ src/main/java/tripplan/repository/PlanRepository.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface PlanRepository {
+
     int addTripPlan(Long memberId, String title);
 
     int addDetailPlan(Long tripPlanId, int contentId);
--- src/main/java/tripplan/service/PlanService.java
+++ src/main/java/tripplan/service/PlanService.java
@@ -1,10 +1,11 @@
 package tripplan.service;
 
-import tripplan.TripPlan;
-
 public interface PlanService {
+
     int addTripPlan(Long memberId, String title);
+
     int addDetailPlan(Long memberId, Long tripPlanId, int contentId);
+
     int updateTripPlan(Long memberId, Long tripPlanId, String title);
 
     int removeDetailPlan(Long memberId, Long detailPlanId);
