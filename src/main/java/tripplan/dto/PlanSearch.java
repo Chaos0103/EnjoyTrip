4b50953700b4522d14c235e925ba28f8e8c31563
leeyr0412 <leeyr0412@naver.com>
2023-04-11 10:12:38 +0900

    fix: 여행 목록 출력 페이징기능 추가

--- src/main/java/tripplan/dto/PlanSearch.java
+++ src/main/java/tripplan/dto/PlanSearch.java
@@ -2,20 +2,13 @@ package tripplan.dto;
 
 import lombok.Builder;
 import lombok.Data;
-import member.Member;
-
-import java.time.LocalDateTime;
 
 @Data
 public class PlanSearch {
-    private Member member;
-    private String title;
-    private LocalDateTime createdDate;
+    private String condition;
 
     @Builder
-    public PlanSearch(Member member, String title, LocalDateTime createdDate) {
-        this.member = member;
-        this.title = title;
-        this.createdDate = createdDate;
+    public PlanSearch(String condition) {
+        this.condition = condition;
     }
 }
